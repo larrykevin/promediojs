52b5946708ba501e79335de9ebf3edb1442ce017
Larry Caycho <larry.kevin.c.c@gmail.com>
2021-07-25 12:40:31 -0500

    Agregando archivos de mediana

---
+++ mediana/mediana.js
@@ -0,0 +1,14 @@
+const lista1 = [
+    100,
+    200,
+    500,
+    4000000,
+];
+
+const mitadLista1 = lista1.length / 2;
+
+if (/* lista 1 es par */) {
+    //necesitamos dos elementos
+    // el promedio
+    // mediana
+}
