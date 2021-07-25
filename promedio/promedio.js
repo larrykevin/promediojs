ca54ae88d808ba86ba306bc605b877e7f37b5874
Larry Caycho <larry.kevin.c.c@gmail.com>
2021-07-25 12:08:20 -0500

    Agregando función para calcular media aritmética

--- promedio/promedio.js
+++ promedio/promedio.js
@@ -1,1 +1,17 @@
-const lista1 = [];
+function calcularMediaAritmetica(lista) {
+    /* let sumaLista = 0;
+    for (let i = 0; i < lista.length; i++) {
+        sumaLista = sumaLista + lista[i];
+    } */
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
+}
+
+console.log(calcularMediaAritmetica([1, 14, 151, 197]))
